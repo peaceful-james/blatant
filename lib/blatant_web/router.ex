66b6ef3036993346d1d222d28a52c3c0e98d8d3b
Peaceful James <james@peacefulprogramming.xyz>
2020-06-21 10:31:28 +0100

    Remove a fully absorbed auto-geenrated comment (advice)

--- lib/blatant_web/router.ex
+++ lib/blatant_web/router.ex
@@ -18,14 +18,6 @@ defmodule BlatantWeb.Router do
     forward "/", Absinthe.Plug, schema: BlatantWeb.Schema
   end
 
-  # Enables LiveDashboard only for development
-  #
-  # If you want to use the LiveDashboard in production, you should put
-  # it behind authentication and allow only admins to access it.
-  # If your application does not have an admins-only section yet,
-  # you can use Plug.BasicAuth to set up some basic authentication
-  # as long as you are also using SSL (which you should anyway).
-
   scope "/" do
     unless Mix.env() in [:dev, :test] do
       pipe_through :autobrowser
